5286f9eebadf5b9ce8aae3382fee09af837292e4
GregorBag <gregorbagd@gmail.com>
2026-09-13 18:02:37 +0300

    add lru and lfu cache

---
+++ lfu-in-cxx/cache.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cstddef>
+#include <list>
+#include <unordered_map>
+
+namespace caches {
+
+template <typename T, typename KeyT = int> struct cache_t { 
+  std::size_t capacity_;
+  std::size_t min_freq_ = 0;
+
+  using KeyList = std::list<KeyT>;
+  using KeyIt = typename KeyList::iterator;
+
+  struct Entry {
+    T value;
+    std::size_t freq;
+    KeyIt position;
+  };
+
+  std::unordered_map<KeyT, Entry> entries_;
+  std::unordered_map<std::size_t, KeyList> groups_;
+
+  explicit cache_t(std::size_t capacity): capacity_(capacity) {}
+
+  //end of data structures
+
+  bool full() const { return (entries_.size() == capacity_); }
+
+  template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
+    if (!capacity_) return false;
+
+    auto hit = entries_.find(key);
+    if (hit != entries_.end()) {
+      auto& entry = hit->second;
+
+      const auto old_freq = entry.freq;
+      auto& old_group = groups_.at(old_freq);
+      auto& new_group = groups_[old_freq + 1];
+
+      new_group.splice(new_group.begin(), old_group, entry.position);
+
+      entry.freq++;
+
+      if (old_group.empty()) {
+        groups_.erase(old_freq);
+
+        if (min_freq_ == old_freq) min_freq_++;
+      }
+
+      return true;
+    }
+
+    T value = slow_get_page(key);
+
+    if (full()) {
+      auto& min_group = groups_.at(min_freq_);
+      KeyT delete_key = min_group.back();
+
+      entries_.erase(delete_key);
+      min_group.pop_back();
+
+      if (min_group.empty()) groups_.erase(min_freq_);
+    }
+
+    auto& group_freq_1 = groups_[1];
+    group_freq_1.emplace_front(key);
+
+    entries_.emplace(key, Entry{value, 1, group_freq_1.begin()});
+
+    min_freq_ = 1;
+
+    return false;
+  }
+
+};
+
+} // namespace caches
---
+++ lru-in-cxx/cache.hpp
@@ -0,0 +1,59 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT ILab
+// Slides: https://sourceforge.net/projects/cpp-lects-rus/files/cpp-graduate/
+// Licensed after GNU GPL v3
+//
+//-----------------------------------------------------------------------------
+//
+//  Example for LRU cache in C++
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstddef>
+#include <list>
+#include <unordered_map>
+#include <utility>
+
+namespace caches { 
+
+template <typename T, typename KeyT = int> struct cache_t {
+  std::size_t sz_;
+  // Each entry is {key, page}; most recently used entry is at the front.
+  std::list<std::pair<KeyT, T>> cache_;
+
+  using ListIt = typename std::list<std::pair<KeyT, T>>::iterator;
+  std::unordered_map<KeyT, ListIt> hash_;
+
+  explicit cache_t(std::size_t sz) : sz_(sz) {}
+
+  std::size_t max_capacity() const { return sz_; }
+  bool full() const { return (cache_.size() == sz_); }
+
+  template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
+    if (max_capacity() == 0)
+      return false;
+
+    auto hit = hash_.find(key);
+    if (hit != hash_.end()) {
+      auto eltit = hit->second;
+      cache_.splice(cache_.begin(), cache_, eltit);
+      return true;
+    }
+
+    // This key is absent, even if the cache contains other pages.
+    T page = slow_get_page(key);
+
+    if (full()) {
+      hash_.erase(cache_.back().first);
+      cache_.pop_back();
+    }
+    cache_.emplace_front(key, page);
+    hash_.emplace(key, cache_.begin());
+    return false;
+  }
+};
+
+} // namespace caches
